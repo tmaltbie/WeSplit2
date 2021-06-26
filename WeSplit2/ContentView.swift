e42c2945bb86862e4c8d4a5ab5eb0fb633f4ce7b
Trevor Maltbie <trevor@Trevors-MacBook-Pro.local>
2021-06-25 20:50:56 -0700

    first commit

---
+++ WeSplit2/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  WeSplit2
+//
+//  Created by Trevor Maltbie on 6/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) people")
+                    }
+                }
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Trevor Maltbie on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
