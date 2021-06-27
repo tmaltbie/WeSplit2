5fa4253a2dbdfe300dd09e8369548d250b93677b
Trevor Maltbie <trevor@worklocal.local>
2021-06-26 17:13:02 -0700

    updated for Day 18

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -14,6 +14,18 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -27,11 +39,22 @@ struct ContentView: View {
                         }
                     }
                 }
-                .navigationTitle("We Split")
+                
+                Section(header: Text("How much do you want to tip?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("Amount per person")){
+                    Text("$\(totalPerPerson, specifier: "%.2f")") // specifier to mean “a two-digit floating-point number.”
+                }
             }
-            Section {
-                Text("$\(checkAmount)")
-            }
+            .navigationTitle("We Split")
+                
         }
     }
 }
