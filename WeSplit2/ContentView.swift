201bbb2c35c2ac632c1aa056941f956fcf7a87c4
Trevor Maltbie <trevor@worklocal.local>
2021-07-04 11:44:22 -0700

    custome modifier for zero tip selection

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -5,17 +5,22 @@
 //  Created by Trevor Maltbie on 6/24/21.
 //
 
+// CHALLENGE:
+    // Use a conditional modifier to change the total amount text view to red if the user selects a 0% tip.
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
+    @State private var textNumOfPeeps = "1"
+    @State private var useRedTextForZeroTip = 4
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(textNumOfPeeps) ?? 0
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
@@ -27,7 +32,7 @@ struct ContentView: View {
     }
     
     var grandTotal: Double {
-        let grandTotal = totalPerPerson * (Double(numberOfPeople + 2))
+        let grandTotal = totalPerPerson * (Double(textNumOfPeeps) ?? 0)
         return grandTotal
     }
     
@@ -42,15 +47,17 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    
-//                    TextField("Number of people", text: $peopleString)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
                 }
+                
+                Section (header: Text("Number of people")){
+                    TextField("Number of People", text: $textNumOfPeeps)
+                    
+                }
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
                 
                 Section(header: Text("How much do you want to tip?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
@@ -61,9 +68,10 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Subtotal of cheque")) {
+                Section(header: Text("grand total:")) {
                     Text("$\(grandTotal, specifier: "%.2f")")
                 }
+                .foregroundColor(tipPercentage == 4 ? .red : .none)
                 
                 Section (header: Text("Amount per person")){
                     Text("$\(totalPerPerson, specifier: "%.2f")") // specifier to mean “a two-digit floating-point number.”
